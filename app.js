b1b9b333603107a5ef518747abdf6b86a681cd61
Nika <freexnick@yahoo.com>
2022-07-19 21:59:48 +0400

    port_update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static("./public"));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
